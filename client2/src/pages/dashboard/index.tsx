80de4df25b72bdef11ed3d48afdcf11e7b4cfb73
开发小猕 <1322147900@qq.com>
2023-11-27 00:00:14 +0800

    学习了TypeScript的接口和类

--- client2/src/pages/dashboard/index.tsx
+++ client2/src/pages/dashboard/index.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import StaffAmount from './components/StaffAmount';
-import { useSelector } from'@umijs/max';
+import OldstffTable from './components/OldstffTable';
+import { useSelector,useDispatch } from'@umijs/max';
+
+
 function index(props) {
 
-  const amountDataList = useSelector(state => state.dashboard);
+  const {amountDataList,staffList} = useSelector(state => state.dashboard);
 
-  console.log(amountDataList,'amountDataList');
+  const dispatch = useDispatch();
   
+  // useEffect( async () => {
+  //   const res = await dispatch({type: "dashboard/initDashboradList"})
+  // },[])
+
   return (
     <div className="dashboard-container">
       {
-        amountDataList.amountDataList.map((item,index )=> {
+       amountDataList.map((item,index )=> {
           return <StaffAmount key={index} {...item} />
         })
       }
+
+      {/* 最老的十个员工 */}
+      <OldstffTable {...staffList}/>
     </div>
   );
 }
--- client2/src/pages/dashboard/model.ts
+++ client2/src/pages/dashboard/model.ts
@@ -1,11 +1,20 @@
+import $http from "api"
+
 export default {
   namespace: "dashboard",
   state: {
     amountDataList: [
-      {title: "总人数", amount: 17, styleData: {width:"100%", height: "170px"}},
-      {title: "入职1年内员工", amount: 17, styleData: {width:"33%", height: "170px"}},
-      {title: "入职1-2年内员工", amount: 0, styleData: {width:"33%", height: "170px"}},
-      {title: "入职3年以上员工", amount: 7, styleData: {width:"33%", height: "170px"}}
-    ]   
+    ],
+    staffList: {
+    },   
+  },
+
+  effects: {
+    *initDashboradList({},{put,call}) {
+      console.log($http,'http');
+      
+      const {data} = yield call($http.analyzeStaff)
+      console.log(data);
+    }
   }
 }
